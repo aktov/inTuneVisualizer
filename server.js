3b5ae09ab42ea3c42f2b80fc5f79f847ce13fea5
Alex Tov <atov@ucsd.edu>
2018-04-22 15:52:51 -0700

    got a working server

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+const handlebars = require('express3-handlebars')
+
 const app = express();
 // ----------------------------------------------------------------- //
 
@@ -11,6 +15,19 @@ app.get('/', index.view);
 
 // ----------------------------------------------------------------- //
 app.use(express.static('static_files/'));
+// all environments
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('IxD secret key'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
   console.log('Server started on http://localhost:3000/');

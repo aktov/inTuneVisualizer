6c735469f729f59c57e1151d71b6139163a1980c
Vanna <vhluu@ucsd.edu>
2018-04-26 17:38:45 -0700

    connected to firebase

---
+++ app/static/js/firebase.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyBsHsIuWG4Tcvn1fMHOoxH_Dp_A2fK1Rco",
+    authDomain: "friendoabjv.firebaseapp.com",
+    databaseURL: "https://friendoabjv.firebaseio.com",
+    projectId: "friendoabjv",
+    storageBucket: "friendoabjv.appspot.com",
+    messagingSenderId: "157387684680"
+  };
+firebase.initializeApp(config);

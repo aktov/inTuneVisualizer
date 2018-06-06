3ea1ac364e89b709f952223816a8e0457661a69e
Alex Tov <atov@ucsd.edu>
2018-06-06 05:12:56 -0700

    Updated to current branch

--- app/static/js/hash.js
+++ app/static/js/hash.js
@@ -1,3 +1,10 @@
+/*
+File: hash.js
+Purpose: Contains a hash function to generate unique user IDs in our database to avoid future collisions
+and a more efficient way to store users in the long term if we were to increase the number of users
+drastically. 
+*/
+
 module.exports = {
   hash: function(str) {
     let hash = 5381,
